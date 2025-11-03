cd18496367e50a8872a05b4ca02ea79e1a98d504
RasmusEriksson <rasmus.eriksson@elev.ga.ntig.se>
2025-11-03 11:12:20 +0100

    added combatlog

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-playerContainer = document.getElementById("player_hp")
-enemyContainer = document.getElementById("enemy_hp")
+const playerContainer = document.getElementById("player_hp")
+const enemyContainer = document.getElementById("enemy_hp")
+
+const startButton = document.querySelector("#start-button")
+
+const logContainer = document.querySelector("#combat-log")
+const logMessage = document.getElementsByClassName("log-message")[0]
 
 const playerName = "mimimi"
-let playerHp = 100
-let enemyHp = 100
 
-let game = true
+function combatlog(msg,type) {
+    const newLog = logMessage.cloneNode(true)
+    const msg_container = newLog.getElementsByClassName("msg")[0]
+    const type_container = newLog.getElementsByClassName("type")[0]
+
+    msg_container.textContent = msg
+    type_container.textContent = type
+
+    logContainer.appendChild(newLog)
+}
+    
 
 // returns a number value between given interval, 
 // min is minimum return and max is maximum return
@@ -14,42 +27,50 @@ function randi_range(min,max) {
     return min + Math.floor(Math.random() * range)
 }
 
-
-//a function which is the main loop for the game
-while (game) {
-    //rolls a 6-sided dice for both player and enemy
-    let player_roll = randi_range(1,6)
-    let enemy_roll = randi_range(1,6)
     
-    //checks if player- or enemyroll is higher, the one who has higher hits the other
-    //if they roll the same they both block and nothing happens
-    if (player_roll > enemy_roll) {
-        console.log("player hits enemy!")
-        enemyHp-= 10
-    }
-    else if (enemy_roll > player_roll) {
-        console.log("enemy hits player!")
-        playerHp -= 10
-    }
-    else {
-        console.log("both block, nothing happens")
-    }
-    playerContainer.textContent = playerHp
-    enemyContainer.textContent = enemyHp
 
-    //Checks if either enemy or player has 0 or less hp, if true then it ends the game
-    if (enemyHp <= 0 || playerHp <= 0) {
-        game = false
-        //prints out the winner depending on which character has the highest hp
+startButton.addEventListener("click",startGame)
 
-        if (enemyHp > playerHp) {
-            console.log("enemy has won!!!!!!!!")
+function startGame() {
+    let playerHp = 100
+    let enemyHp = 100
+    //a function which is the main loop for the game
+    let game = setInterval(() =>{
+        //rolls a 6-sided dice for both player and enemy
+        let player_roll = randi_range(1,6)
+        let enemy_roll = randi_range(1,6)
+        
+        //checks if player- or enemyroll is higher, the one who has higher hits the other
+        //if they roll the same they both block and nothing happens
+        if (player_roll > enemy_roll) {
+            combatlog("player hits enemy for:  " + String(player_roll),"Good")
+            enemyHp-= player_roll
+        }
+        else if (enemy_roll > player_roll) {
+            combatlog("enemy hits player for:  " + String(enemy_roll),"Bad")
+            playerHp -= enemy_roll
         }
         else {
-            console.log("player has won!!!!!!!")
+            combatlog("Both block, nothing happens","Neautral")
         }
-    }
+        playerContainer.textContent = playerHp
+        enemyContainer.textContent = enemyHp
+
+        //Checks if either enemy or player has 0 or less hp, if true then it ends the game
+        if (enemyHp <= 0 || playerHp <= 0) {
+            clearInterval(game)
+            //prints out the winner depending on which character has the highest hp
+
+            if (enemyHp > playerHp) {
+                combatlog("ENEMY WINS!","Terrible")
+            }
+            else {
+                combatlog("PLAYER WINS!","Incredible")
+            }
+        }
+    },150)
 }
+
 
 
 
